5387dd3a521b75032b0dd6806f156c3d380ec1e7
Chellie-code <mimirekon9@gmail.com>
2022-03-14 12:12:45 +0300

    added content

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-let usersAge = propmt('Enter your age.');
+let usersName=prompt('What is your name?')
+let usersAge = prompt('Enter your age.'); 
+if((usersAge%2)===0){
+    alert(`${usersName} Your age is even`)
+}else if((usersAge%2)===1){
+    alert(`${usersName} Your age is odd`)
+}else{
+    alert(`${usersName} Please enter correct age`)
+}
